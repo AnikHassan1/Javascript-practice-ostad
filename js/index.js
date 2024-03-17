355b786cf88fccc5371f74b4fee6f2f919d02283
Anik Hassan1 <ah491606@gmail.com>
2024-03-18 00:54:27 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// document.write("Hello World");
+// let x=10;
+// let y=20;
+// console.log(x+y);
+
+let a=263;
+
+if(a<33){
+    console.log("fail");
+}  
+else if(a>=33 && a <40){
+    console.log("Grade D"); 
+}
+else if(a>=40 && a <50){
+    console.log("Grade C"); 
+}
+else if(a>=50 && a <60){
+    console.log("Grade B"); 
+}
+else if(a>=60 && a <70){
+    console.log("Grade A-"); 
+}
+else if(a>=70 && a <80){
+    console.log("Grade A"); 
+}
+else if(a>=80 && a <100){
+    console.log("Grade A+"); 
+}
+else{
+    console.log("Please Enter Number 1 To 100");
+}
